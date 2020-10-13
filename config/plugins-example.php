62ca3cbef3e6143a1482598677d06567523322ca
Fredrik Johansson <jeanfredrik@gmail.com>
2020-10-13 11:15:53 +0200

    📝 Add reCAPTCHA example config

--- config/plugins-example.php
+++ config/plugins-example.php
@@ -9,3 +9,9 @@ define('ACF_LITE', false);
  * Share search notices across the network
  */
 define('SEARCH_NOTICES_NETWORK', true);
+
+/**
+ * Recaptcha
+ */
+define('G_RECAPTCHA_KEY', '');
+define('G_RECAPTCHA_SECRET', '');
